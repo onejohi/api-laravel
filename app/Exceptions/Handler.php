d0a9e8d8c3e03afbafa1cf7b6e5cd73a387bfe63
Onejohi <tonniewanjohi@gmail.com>
2021-02-01 19:35:00 +0300

    - add sanctum for authentication

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,6 +38,10 @@ class Handler extends ExceptionHandler
     public function report(Throwable $exception)
     {
         parent::report($exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception) {
+        return response()->json(['error' => 'Unauthenticated'], 401);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/dashboard', function(Request $request, Response $response) {
+    return response()->json([
+        'status_code' => 200,
+        'message' => 'okay'
+    ]);
+});
+
+Route::post('signin', 'SignInController@signIn');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+    return ['token', $token->plainTextToken];
+});
+
+Route::get('test', function (Request $request) {
+    return $request;
 });
